6480409ad8f2e0d91b28fcf241053d85ea88d6e9
Office Tracker <officetracker@gmail.com>
2026-06-03 18:27:57 +0530

    mobile

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
 });
 
 // Re-compile trigger: 2026-04-30T12:42:00
